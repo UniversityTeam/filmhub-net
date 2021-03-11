ee77a3f6dd44275eb8213ff67bcc74b00bc22ff1
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-03-11 23:50:59 +0200

    Create Server.cs

---
+++ src/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filmhub
+{
+    class Server
+    {
+        List<Session> sessions;
+        Dictionary<Session, User> users;
+
+        string RootFolder;
+
+
+        public Server(string RootFolder)
+        {
+            this.RootFolder = RootFolder;
+            Console.WriteLine("Server started");
+        }
+    }
+}
