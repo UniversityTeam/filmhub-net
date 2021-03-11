2584a8027f4c6ba81b7319c6276c7ef3fa975ed5
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-03-11 23:11:09 +0200

    Added film

---
+++ src/Film.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filmhub
+{
+    public class Film
+    {
+        int id;
+        string title;
+        int year;
+        float rating;
+        int rating_count;
+        int views;
+        string poster;
+        Genre genre;
+        string url;
+        bool free;
+
+        public Film(
+            int id,
+            string title,
+            int year,
+            float rating,
+            int rating_count,
+            int views,
+            string poster,
+            Genre genre,
+            string url,
+            bool free
+        ) {
+            this.id = id;
+            this.title = title;
+            this.year = year;
+            this.rating = rating;
+            this.rating_count = rating_count;
+            this.views = views;
+            this.poster = poster;
+            this.genre = genre;
+            this.url = url;
+            this.free = free;
+        }
+    }
+}
