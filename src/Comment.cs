c61b991b843a33426a9e0667155b5bf573ed70b7
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-03-11 23:10:54 +0200

    Add comments

---
+++ src/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filmhub
+{
+    class Comment
+    {
+        int id { get; set; }
+        int filmid { get; set; }
+        int userid { get; set; }
+
+        string textdata { get; set; }
+        bool approved { get; set; }
+
+        public Comment(
+            int id, 
+            int filmid, 
+            int userid, 
+            string textdata, 
+            bool approved
+        ) {
+            this.id = id;
+            this.filmid = filmid;
+            this.userid = userid;
+            this.textdata = textdata;
+            this.approved = approved;
+        }
+    }
+}
