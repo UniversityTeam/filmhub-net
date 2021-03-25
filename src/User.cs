c474aadcedf1d693b09f74531737e410c3a5dbb6
Storkki <65562829+Storkki@users.noreply.github.com>
2021-03-25 10:51:35 +0300

    Update User.cs

--- src/User.cs
+++ src/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +6,12 @@ namespace Filmhub
 {
     public class User
     {
-        int id { get; set; }
-        int session { get; set; }
+        public int id { get; set; }
+        public int session { get; set; }
         string login { get; set; }
         string hash { get; set; }
-        bool moderator { get; set; }
-        bool administrator { get; set; }
+        public bool moderator { get; set; }
+        public bool administrator { get; set; }
 
         public User(int id, int session, string login, string hash, bool moderator, bool administrator)
         {
@@ -21,13 +21,24 @@ namespace Filmhub
             this.hash = hash;
             this.moderator = moderator;
             this.administrator = administrator;
+            Console.WriteLine("Created new user with arguments.");
         }
+
+        public User()
+        {
+            Console.WriteLine("Created new user without arguments.");
+        }
+
         public void AddFilm() { }
         public void RemoveFilm() { }
         public void LeaveComment() { }
         public void RateFilm() { }
         public void AddModerator() { }
-        public void AddAdministrator() { }
+        public void AddAdministrator(User user) 
+        {
+            user.administrator = true;
+            Console.WriteLine($"User with id = {user.id} now is Administrator!");
+        }
         public void RemoveComment() { }
         public void RemoveUser() { }
         public void ShowRecommendations() { }
