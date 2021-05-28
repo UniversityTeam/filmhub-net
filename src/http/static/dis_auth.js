416d5c9fd0b96256b283d7c95cebedae4fc34769
Bohdan Maik <65562829+Storkki@users.noreply.github.com>
2021-05-28 18:16:44 +0300

    Delete src directory

--- src/http/static/dis_auth.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const DisableAuth = async () => {
-  const promise = makeRequest('{}', 'POST', '/islogin');
-  promise.then(res => {
-    if (res === 'true') {
-      document.querySelectorAll('.auth-block').forEach(el => el.remove());
-    }
-  }).catch(err => console.log(err));
-};
